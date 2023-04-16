7d638351a5b1ed935d9985e03d4792e5df014ca0
Riccardo Strina <riccardo.strina@icloud.com>
2023-04-16 20:02:13 +0200

    Add stop and start commands

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from telegram import __version__ as TG_VER
 from santa import Santa
 import secret
+from status import Status
+import time
 
 
 try:
@@ -19,24 +21,42 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 
 # class with the stuff
 santa = Santa()
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=ForceReply(selective=True),
-    )
+status = Status()
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
+async def stop_toggle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    status.stop()
+
+    await update.message.reply_text("Pausa, se avete bisogno chiedete a Matteo")
+
+async def start_toggle(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    status.restart()
+
+    await update.message.reply_text("Sono tornato! Dove sono gli HPPS?")
+
+async def status_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    res = status.get_status()
+
+    text = f"Santa in questo momento e' {'in ascolto' if res[0] else 'in pausa'}\n"
+    text += f"Santa è arrivato alle {time.ctime(res[1])}\n"
+
+    if res[2]:
+        text += f"E' ha fatto pausa alle {time.ctime(res[2])}\n"
+
+    if res[3]:
+        text += f"E' tornato alle {time.ctime(res[3])}"
+
+    await update.message.reply_text(text)
+
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not status.running:
+        return 
+    
     msg = update.message.text.lower()
 
     for trigger in Santa.GREATINGS:
@@ -70,9 +90,9 @@ def main() -> None:
     application = Application.builder().token(secret.TOKEN).build()
 
     # on different commands - answer in Telegram
-    #application.add_handler(CommandHandler("start", start))
-    #application.add_handler(CommandHandler("help", help_command))
-    #application.add_handler(CommandHandler("quit", stop_toggle))
+    application.add_handler(CommandHandler("stop", stop_toggle))
+    application.add_handler(CommandHandler("start", start_toggle))
+    application.add_handler(CommandHandler("status", status_command))
 
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
